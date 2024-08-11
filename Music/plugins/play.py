eb7a79f08e34647b5fd35e1063f04fcf64b02453
fouadsalkini <fouad@sync.com.lb>
2024-08-11 15:40:32 +0300

    updated error msg

--- Music/plugins/play.py
+++ Music/plugins/play.py
@@ -135,7 +135,7 @@ async def play_music(_, message: Message, context: dict):
         await hell.edit("Searching ...")
         result = await ytube.get_data(query, False)
     except Exception as e:
-        return await hell.edit(f"**Error:**\n`{e}`")
+        return await hell.edit(f"**Error getting data:**\n`{e}`")
     context = {
         "chat_id": message.chat.id,
         "user_id": message.from_user.id,
