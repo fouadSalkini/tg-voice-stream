71da5d19564d5bfdc497205332792fe5817f9eee
fouadsalkini <fouad@sync.com.lb>
2024-08-12 18:05:52 +0300

    added downloader

--- download.py
+++ download.py
@@ -1,9 +1,13 @@
-from Music.utils.youtube import ytube
-video_id = "rCC70UbMuFY"
+import yt_dlp
 
+id = "rCC70UbMuFY"
+video_url = 'https://www.youtube.com/watch?v=rCC70UbMuFY'
+ydl_opts = {
+    'format': 'best',
+    "outtmpl": "downloads/%(id)s.%(ext)s",
+    'username': 'your_email@gmail.com',
+    'password': 'your_password',
+}
 
-async def dwn(id):
-    file_path = await ytube.download(id, True, False)
-    return file_path
-
-dwn(video_id)
+with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([video_url])
