902e39af76532d12e2cccbd5fb484343413a69d9
fouadsalkini <fouad@sync.com.lb>
2024-08-13 13:27:08 +0300

    debug

--- download.py
+++ download.py
@@ -3,14 +3,16 @@ from config import Config
 
 id = "rCC70UbMuFY"
 video_url = 'https://www.youtube.com/watch?v=rCC70UbMuFY'
+authconfig = {
+            "username": Config.YT_USERNAME,
+            "password": Config.YT_PASSWORD,
+            "cookies": Config.YT_COOKIEFILE,
+            "user_agent": Config.YT_USERAGENT
+        }
 ydl_opts = {
     'format': 'best',
-    "outtmpl": "downloads/%(id)s.%(ext)s",
-    'username': Config.YT_USERNAME,
-    'password': Config.YT_PASSWORD,
-    'user_agent': Config.YT_USERAGENT,
-    'cookiefile': Config.YT_COOKIEFILE,
-}
+    "outtmpl": "downloads/%(id)s.%(ext)s"
+}.update(authconfig)
 
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download([video_url])
