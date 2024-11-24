7091ce6775e636eb213d6675918f8ae0ef46c427
fouadsalkini <fouad@sync.com.lb>
2024-11-24 03:01:27 +0200

    download

--- download.py
+++ download.py
@@ -37,21 +37,23 @@ AUTHORIZED_USER_FILE = 'authorized_user.json'
 ##############################
 ##############################
 ## playlists: 
-#1 86 done https://www.youtube.com/watch?v=KsG5KtGSZm8&list=PLr7X7rIaev6LAsGDoVZrDBTWmyXeD0L7h
-#2 23 done https://www.youtube.com/playlist?list=PLWA9mQ1O5nGWrtDqAsdRT2ObEZ7xmOucd
-#3 113 done https://www.youtube.com/playlist?list=PLGbzILZDD0i9eFWuQ3JO1y_pwMYqDBsRY
-#4 165 https://www.youtube.com/playlist?list=PLGbzILZDD0i-FvgUWhs73CG7dUrphqfVF
+#1 86 [done] https://www.youtube.com/watch?v=KsG5KtGSZm8&list=PLr7X7rIaev6LAsGDoVZrDBTWmyXeD0L7h
+#2 23 [done] https://www.youtube.com/playlist?list=PLWA9mQ1O5nGWrtDqAsdRT2ObEZ7xmOucd
+#3 113 [done] https://www.youtube.com/playlist?list=PLGbzILZDD0i9eFWuQ3JO1y_pwMYqDBsRY
+#4 165 [done] https://www.youtube.com/playlist?list=PLGbzILZDD0i-FvgUWhs73CG7dUrphqfVF
 #5 700 https://www.youtube.com/watch?v=e-9dVwLWiAo&list=PLGbzILZDD0i-92GW5L8HKRu2Owdjw6n1L
 #6 1282 https://www.youtube.com/playlist?list=PLYhaCT6xCcqkRESabizGrRnUJCJEorsXx
-#7 10 https://www.youtube.com/watch?v=AS-iUTQ_VqE&list=PLr7X7rIaev6IPBW6q9oPWAsOzAmqLPey_
-#8 31 https://www.youtube.com/watch?v=7yfZXDf7y9U&list=PLyKSKLsF2ArSP_JUyUlztenakNtVaj4Nz
-#9 19 https://www.youtube.com/watch?v=9tlzYANgbcQ&list=PLJdyh1foKTw02WimqlLja3YwWeqTuOepi
-#10 11 https://www.youtube.com/watch?v=IGlVZIxLGKE&list=PLcJLYVXnDZbPqsGSkILg4W0wv5gqvk2b9
-#11 21 https://www.youtube.com/watch?v=fZxVXXNycGk&list=PL9DwJ7DL3FOuAVRKYVw_6SMRiH3yEC7rz
-#12 29 https://www.youtube.com/watch?v=AoeVUPy5e6c&list=PL3lnNg8CXAt_iRC6hHz6ocgvUg4K3E3NA
-#13 17 https://www.youtube.com/watch?v=aBVEiiVU7tQ&list=PL3451BCE803EC9119
-#14 43 https://www.youtube.com/watch?v=BuTM5lIlBjw&list=PL3lnNg8CXAt9DvQtTHAE7YXg6JSle_gu9
-#15 20 https://www.youtube.com/playlist?list=PLQtPEQcFOMBax0xP7ZPYp8IkAmiTPw1WQ
+#7 10 [done] https://www.youtube.com/watch?v=AS-iUTQ_VqE&list=PLr7X7rIaev6IPBW6q9oPWAsOzAmqLPey_
+#8 31 [done] https://www.youtube.com/watch?v=7yfZXDf7y9U&list=PLyKSKLsF2ArSP_JUyUlztenakNtVaj4Nz
+#9 19 [done] https://www.youtube.com/watch?v=9tlzYANgbcQ&list=PLJdyh1foKTw02WimqlLja3YwWeqTuOepi
+#10 11 [done] https://www.youtube.com/watch?v=IGlVZIxLGKE&list=PLcJLYVXnDZbPqsGSkILg4W0wv5gqvk2b9
+#11 21 [done] https://www.youtube.com/watch?v=fZxVXXNycGk&list=PL9DwJ7DL3FOuAVRKYVw_6SMRiH3yEC7rz
+#12 29 [done] https://www.youtube.com/watch?v=AoeVUPy5e6c&list=PL3lnNg8CXAt_iRC6hHz6ocgvUg4K3E3NA
+#13 17 [done] https://www.youtube.com/watch?v=aBVEiiVU7tQ&list=PL3451BCE803EC9119
+#14 43 [done] https://www.youtube.com/watch?v=BuTM5lIlBjw&list=PL3lnNg8CXAt9DvQtTHAE7YXg6JSle_gu9
+#15 20 [done] https://www.youtube.com/playlist?list=PLQtPEQcFOMBax0xP7ZPYp8IkAmiTPw1WQ
+
+# tar --remove-files --exclude=archive_20.tar.gz -czvf archive_20.tar.gz .
 
 # yt-dlp --cookies ../youtube.com_cookies.txt -f bestaudio --extract-audio --audio-format opus --audio-quality 64K -o "%(title)s.%(ext)s" "URL"
 # --playlist-items 1,2,3,...
